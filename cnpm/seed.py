4811ef55799aee6228ba42bf6a71cafabe1c737f
van hoang <prozxhoang@gmail.com>
2025-09-15 14:16:41 +0700

    Update seed.py

--- cnpm/seed.py
+++ cnpm/seed.py
@@ -2,12 +2,10 @@ from models import SessionLocal, User
 
 def run_seed():
     session = SessionLocal()
-    print("👉 Đang thêm dữ liệu...")
+    print("Đang thêm dữ liệu...")
 
-    # Xóa dữ liệu cũ
     session.query(User).delete()
 
-    # Thêm dữ liệu mới
     users = [
         User(name="Nguyen Van A", email="a@example.com"),
         User(name="Tran Thi B", email="b@example.com"),
@@ -16,7 +14,8 @@ def run_seed():
 
     session.add_all(users)
     session.commit()
-    print("✅ Seed dữ liệu thành công!")
+    print("Seed dữ liệu thành công!")
 
 if __name__ == "__main__":
     run_seed()
+
