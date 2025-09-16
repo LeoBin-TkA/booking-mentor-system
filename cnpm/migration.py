9b99e5d06defcf8e43b464d517bdca3e867628fe
van hoang <prozxhoang@gmail.com>
2025-09-16 10:54:31 +0700

    Delete cnpm directory

--- cnpm/migration.py
+++
@@ -1,9 +0,0 @@
-from models import Base, engine
-
-def run_migration():
-    print("👉 Đang tạo bảng...")
-    Base.metadata.create_all(engine)
-    print("✅ Tạo bảng thành công!")
-
-if __name__ == "__main__":
-    run_migration()
